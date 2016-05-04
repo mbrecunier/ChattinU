3b374da96657a4939567f86bf2e84515f90feea5
michelle brecunier and tyler suderman <tcsuder@gmail.com>
2016-05-04 14:37:13 -0700

    create initial files for chat app

---
+++ app/src/main/java/com/example/guest/chattinu/models/User/User.java
@@ -0,0 +1,20 @@
+package com.example.guest.chattinu.models.User;
+
+/**
+ * Created by Guest on 5/4/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User () {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+
+    public String getEmail() { return email; }
+}
